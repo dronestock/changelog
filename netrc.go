246c0d97f2c69e2cd866fba12269cf754a84d224
Storezhang <storezhang@gmail.com>
2022-04-14 21:52:06 +0800

    refactor(全局): 将所有Git操作替换为goexl/git外部库方法调用

--- netrc.go
+++ netrc.go
@@ -1,36 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"strings"
 
-	"github.com/goexl/gox"
-	"github.com/goexl/gox/field"
+	"github.com/goexl/git"
 )
-
-const netrcConfigFormatter = `
-machine %s
-login %s
-password %s
-`
 
 func (p *plugin) netrc() (undo bool, err error) {
 	if undo = `` == strings.TrimSpace(p.Username) || `` == strings.TrimSpace(p.Password); undo {
 		return
 	}
-
-	netrcFilepath := filepath.Join(os.Getenv(homeEnv), netrcFilename)
-	netrcConfig := fmt.Sprintf(netrcConfigFormatter, p.Machine, p.Username, p.Password)
-	netrcFields := gox.Fields{
-		field.String(`machine`, p.Machine),
-		field.String(`username`, p.Username),
-	}
-	if err = ioutil.WriteFile(netrcFilepath, []byte(netrcConfig), defaultFilePerm); nil != err {
-		p.Error(`写入授权文件出错`, netrcFields.Connect(field.Error(err))...)
-	}
+	err = git.Auth(git.Netrc(p.Remote, p.Username, p.Password))
 
 	return
 }
