4ff2cd2cc96e098ee8c8d8b92fb0945a26fdd914
张川 <storezhang@gmail.com>
2023-01-13 14:46:54 +0800

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,3 @@
-FROM quay.io/git-chglog/git-chglog:0.15.1 AS chglog
-
-
-
-
 FROM storezhang/alpine:3.16.2
 
 
@@ -10,13 +5,12 @@ LABEL author="storezhang<华寅>" \
 email="storezhang@gmail.com" \
 qq="160290688" \
 wechat="storezhang" \
-description="Drone持续集成Changelog插件，主要是用来生成改变日志，并结合发布插件发布产品"
+# TODO 增加描述信息
+description="Drone持续集成Docker插件，增加以下功能：1、xxx；2、xxx"
 
 
 # 复制文件
-COPY docker /
-COPY changelog /bin
-COPY --from=chglog /usr/local/bin/git-chglog /usr/local/bin/git-chglog
+COPY plugin /bin
 
 
 RUN set -ex \
@@ -24,24 +18,17 @@ RUN set -ex \
     \
     \
     && apk update \
-    \
-    # 改变日志依赖于Git查询提交记录
-    && apk --no-cache add git \
+    && apk --no-cache add docker \
     \
     \
     \
     # 增加执行权限
-    && chmod +x /bin/changelog \
+    && chmod +x /bin/plugin \
     \
     \
     \
     && rm -rf /var/cache/apk/*
 
 
-# 默认参数
-ENV PLUGIN_CONFIG "exp:file(\"/etc/changelog/config.yml.gohtml\")"
-ENV PLUGIN_TEMPLATE "exp:file(\"/etc/changelog/CHANGELOG.tpl.md\")"
-
-
 # 执行命令
-ENTRYPOINT /bin/changelog
+ENTRYPOINT /bin/plugin
--- plugin.go
+++ plugin.go
@@ -9,37 +9,9 @@ import (
 type plugin struct {
 	drone.Base
 
-	// 源文件
-	Source string `default:"${SOURCE=.}"`
-	// 输出
-	Output string `default:"${OUTPUT=CHANGELOG.md}" validate:"required"`
-	// 开始版本
-	From string `default:"${FROM}"`
-	// 结束版本
-	To string `default:"${TO}"`
-	// 当前版本
-	Tag string `default:"${TAG}"`
-	// 下个版本
-	Next string `default:"${NEXT}"`
-
-	// 仓库地址
-	Url string `default:"${URL=${DRONE_REPO_LINK}}"`
-	// 配置
-	Conf string `default:"${CONFIG}"`
-	// 样式
-	Style string `default:"${STYLE=github}"`
-	// 主题
-	Subject string `default:"# 更新历史 \n\n"`
-	// 标题
-	Title title `default:"${TITLE}"`
-	// 可以导出的类型
-	Types []string `default:"['feat', 'fix', 'pref', 'refactor', 'chore']"`
-	// 模板
-	Template string `default:"${TEMPLATE}"`
-	// 路径
-	Filepath filepath `default:"${FILEPATH}"`
-	// 额外配置
-	Jira *jira `default:"${JIRA}"`
+	// TODO 配置项，可以使用结构体
+	// 如何配置选项请参数：https://github.com/dronestock/drone
+	Todo string `default:"${TODO=默认值}" validate:"required"`
 }
 
 func newPlugin() drone.Plugin {
@@ -50,15 +22,16 @@ func (p *plugin) Config() drone.Config {
 	return p
 }
 
+// Steps TODO 返回所有要执行步骤
 func (p *plugin) Steps() drone.Steps {
 	return drone.Steps{
-		drone.NewStep(newBuildStep(p)).Name("生成").Build(),
-		drone.NewStep(newCleanupStep(p)).Name("清理").Build(),
+		drone.NewStep(p.todo, drone.Name(`启动守护进程`)),
 	}
 }
 
+// Fields TODO 这儿返回所有的参数，上层在执行步骤时，会将参数在日志中打印
 func (p *plugin) Fields() gox.Fields[any] {
 	return gox.Fields[any]{
-		field.New("output", p.Output),
+		field.New("todo", p.Todo),
 	}
 }
